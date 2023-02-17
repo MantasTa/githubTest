281e3b894a5b6d6157fc52e6aab8a79f1a3456b2
MantasTa <125612571+MantasTa@users.noreply.github.com>
2023-02-17 02:59:13 +0200

    idiegta f3 framework ir wp

---
+++ f3/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'bcosca/fatfree-core',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b807db9baa593c6d9df032bde7487e3aebdf18b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree' => array(
+            'pretty_version' => '3.7.3',
+            'version' => '3.7.3.0',
+            'reference' => '62f40d88994b99a51cfba5556931e80e1bb64a8c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b807db9baa593c6d9df032bde7487e3aebdf18b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
